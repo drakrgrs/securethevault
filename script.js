521da9c03c78ae8615b99e9ea97f531b67926f75
Drake Rogers <drakrgrs@gmail.com>
2024-07-22 07:43:43 -1000

    secret code

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+
+    1.Create three different variables each with their own operation.
+    2.Make sure each var equates to the codes in the "secret code"
+    3.I will add comments throughout the code in js
+    4.Create an alert to populate on the browser (i.e. Dialog Box)
+
+
+    */
+
+    const num1= 3+2+4+1; //Combo 1//
+    const num2= 8*5; //Combo 2//
+    const num3= (80/2)-1; //Combo 3//
+    alert(num1+ '-' + num2 + '-' + num3);
